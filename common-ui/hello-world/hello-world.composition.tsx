647613e3b233425c5aa0bbe301da76ba870b98eb
mrgiao1995 <mrgiao1995@outlook.com>
2023-06-25 17:44:08 +0700

    Update component

--- common-ui/hello-world/hello-world.composition.tsx
+++ common-ui/hello-world/hello-world.composition.tsx
@@ -3,6 +3,6 @@ import { HelloWorld } from './hello-world';
 
 export const BasicHelloWorld = () => {
   return (
-    <HelloWorld>hello world!!</HelloWorld>
+    <HelloWorld>hello world!!!</HelloWorld>
   );
 }
